b5d49854713a7eccb435ed655906d035afcd3657
Robert Osial <rob.osial@gmail.com>
2022-01-27 09:08:02 +0000

    added navigation to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-import Tasks from "./features/tasks/Tasks.js";
+import { HashRouter, Switch, Route, Link, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage.js";
+import TasksPage from "./features/tasks/TasksPage/index.js";
+import TaskPage from "./features/tasks/TasksPage/TaskPage/index.js";
 
-const App = () => <Tasks />
+
+const App = () =>
+
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">o autorze</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/zadania/:id">
+        <TaskPage />
+      </Route>
+      <Route path="/zadania">
+        <TasksPage />
+      </Route>
+      <Route path="/autor">
+        <AuthorPage />
+      </Route>
+      <Route path="/">
+        <Redirect to="/zadania" />
+      </Route>
+    </Switch>
+  </HashRouter>
 
 export default App;
