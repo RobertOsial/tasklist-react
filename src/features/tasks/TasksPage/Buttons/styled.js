d44b10493e3d27c800547117f42241109504779f
Robert Osial <rob.osial@gmail.com>
2022-01-27 09:10:18 +0000

    adjusted Buttons Wrapper on small devices

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -4,4 +4,9 @@ export const Wrapper = styled.div`
    display: flex;
    gap: 20px;
    flex-wrap: wrap;
+   justify-content: flex-end;
+
+   @media(max-width: ${({theme}) => theme.breakpoint.mobileSmall}px) {
+       gap: 5px;
+   }
 `;
